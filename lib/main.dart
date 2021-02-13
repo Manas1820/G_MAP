3e1fa343ea4a5774dd8f5e0a6943bfe824c3a171
Manas1820 <manasgupta1820@gmail.com>
2021-02-14 04:37:57 +0530

    🎇Login Screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
+import 'package:connectivity_wrapper/connectivity_wrapper.dart';
 import 'package:flutter/material.dart';
-import 'package:hack_app/models/custom_drawer/CustomDrawer.dart';
+import 'package:hack_app/screens/loginScreen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return ConnectivityAppWrapper(
+        app: MaterialApp(
       // routes: {
       //   HomeScreen.id: (context) => HomeScreen(),
       //   ReceiptData.id: (context) => ReceiptData(),
       // },
       // initialRoute: HomeScreen.id,
-      home: CustomDrawer(),
-    );
+      home: GauthPage(),
+    ));
   }
 }
